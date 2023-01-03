a7e9c3dee53bf85e13ce258f48d39691509ebb78
rangersui <2506488479@qq.com>
2023-01-03 15:23:20 +0800

    初次提交，引入服务端代码和peerjs库

---
+++ README.md
@@ -0,0 +1,12 @@
+# peerjs-practice
+
+webrtc practice based on peerjs
+
+## dependencies
+
+dependencies are listedd in package.json
+Remember to use npm install
+
+## SSL certificates
+
+webrtc is forced to use ssl, so use openssl to generate a self-signed certificate.
---
+++ src/server.js
@@ -0,0 +1,25 @@
+// 导入包 
+var fs = require('fs');
+var PeerServer = require('peer').PeerServer;
+var https = require('https');
+var serveIndex = require('serve-index');
+var express = require("express");
+
+var options = {
+  key: fs.readFileSync('key/server.key'),
+  cert: fs.readFileSync('key/server.crt')
+}
+
+var server = PeerServer({
+  port: 9000,
+  ssl: options,
+  path: "/"
+});
+
+/** 构建html页 */
+var htmlApp = express();
+htmlApp.use(serveIndex('./html'));
+htmlApp.use(express.static("./html"))
+var httpsServer = https.createServer(options, htmlApp);
+httpsServer.listen(81, "0.0.0.0");
+
